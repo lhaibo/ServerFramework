d766d8657c50aea497f68c6e1806be223ba8cb76
lhaibo <1170946850@qq.com>
2020-07-04 13:21:18 +0800

    添加protobuf

---
+++ Server/Protobuf/build.bat
@@ -0,0 +1,2 @@
+protoc --csharp_out=E:\桌面\UnityTestDemo\SocketDemo\Server\Server\Server SocketDemoProtocol.proto
+protoc --csharp_out=E:\桌面\UnityTestDemo\SocketDemo\Assets\Scripts SocketDemoProtocol.proto
